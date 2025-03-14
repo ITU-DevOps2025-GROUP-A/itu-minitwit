68dd17410bc277a04b1b7ab5edea96fef5ce4f65
JacquesPuvis <japu@itu.dk>
2025-03-14 21:03:28 +0100

    created logic for PrivateTimeLine

--- itu-minitwit/Web/Web/Components/Pages/PrivateTimeline.razor.cs
+++ itu-minitwit/Web/Web/Components/Pages/PrivateTimeline.razor.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Web.Services;
+using Web.Services.DTO_s;
 
 namespace Web.Components.Pages;
 
 public partial class PrivateTimeline : ComponentBase
 {
     [Parameter] public string Author { get; set; }
+    
+    public IEnumerable<DisplayMessageDto> messages { get; set; }
+    [Inject] private IMessageService messageService { get; set;  }
+
+    protected override async Task OnInitializedAsync()
+    { 
+        messages = await messageService.GetAuthorMessages(Author);
+    }
 }
