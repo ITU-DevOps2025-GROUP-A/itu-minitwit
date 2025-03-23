cde05e2c4928d45a22dee8002bb3256f0717ec2f
Mathias <math045b@gmail.com>
2025-03-23 21:06:27 +0100

    Create MetricsConfig.cs

---
+++ itu-minitwit/Api/Api/MetricsConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Api;
+
+public class MetricsConfig
+{
+    public const string ServiceName = "minitwit-api";
+    public const string ServiceVersion = "1.0.0";
+    static readonly Meter Meter = new (ServiceName, ServiceVersion);
+}
