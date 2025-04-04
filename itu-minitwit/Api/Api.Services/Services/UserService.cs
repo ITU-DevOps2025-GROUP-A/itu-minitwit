8a4f3b23ff61a531aeb6bae977a5129aa523df4b
RasmusLarsen02 <rasmus.rosenmejer@gmail.com>
2025-04-04 14:09:22 +0200

    Implemented adding users from UserDoesntExistException

--- itu-minitwit/Api/Api.Services/Services/UserService.cs
+++ itu-minitwit/Api/Api.Services/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using Api.Services.Dto_s;
+using Api.Services.Exceptions;
 using Api.Services.LogDecorator;
 using Api.Services.RepositoryInterfaces;
 
@@ -8,9 +9,11 @@ public interface IUserService
 {
     public Task<ReadUserDTO> Register(CreateUserDTO createUserDto);
     public Task<bool> Login(LoginUserDTO dto);
+    public Task RegisterUsersFromException(string username, string follow, UserDoesntExistException e);
+    public Task RegisterUserFromException(string username, UserDoesntExistException e);
 }
 
-public class UserService(IUserRepository userRepository) : IUserService
+public class UserService(IUserRepository userRepository, MetricsConfig metrics) : IUserService
 {
     [LogTime]
     [LogMethodParameters]
@@ -26,5 +29,41 @@ public class UserService(IUserRepository userRepository) : IUserService
     public Task<bool> Login(LoginUserDTO dto)
     {
         return userRepository.Login(dto);
+    }
+
+    public async Task RegisterUsersFromException(string username, string follow, UserDoesntExistException e)
+    {
+        if (!e.FollowExists)
+        {
+            await userRepository.Register(new CreateUserDTO
+            {
+                Email = $"{follow}@email.com",
+                Username = follow,
+                Pwd = "TestTest"
+            });
+            metrics.CreateFaultyFollow.Add(1);
+        }
+
+        if (!e.UserExists)
+        {
+            await userRepository.Register(new CreateUserDTO
+            {
+                Email = $"{username}@email.com",
+                Username = username,
+                Pwd = "TestTest"
+            });
+            metrics.CreateFaultyFollower.Add(1);
+        }
+    }
+
+    public async Task RegisterUserFromException(string username, UserDoesntExistException e)
+    {
+        await userRepository.Register(new CreateUserDTO
+        {
+            Username = username,
+            Pwd = "TestTest",
+            Email = $"{username}@email.com",
+        });
+        metrics.CreateFaultyFollower.Add(1);
     }
 }
