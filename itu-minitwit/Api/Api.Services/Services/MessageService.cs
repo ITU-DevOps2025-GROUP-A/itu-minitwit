958f36143ae2892839e84df337803be043c5f492
RasmusLarsen02 <rasmus.rosenmejer@gmail.com>
2025-04-04 14:10:44 +0200

    Handles UserDoesntExistException
    Creates users when erros occures

--- itu-minitwit/Api/Api.Services/Services/MessageService.cs
+++ itu-minitwit/Api/Api.Services/Services/MessageService.cs
@@ -1,4 +1,5 @@
 using Api.Services.Dto_s.MessageDTO_s;
+using Api.Services.Exceptions;
 using Api.Services.LogDecorator;
 using Api.Services.RepositoryInterfaces;
 
@@ -13,7 +14,7 @@ public interface IMessageService
     public Task<bool> PostMessage(string username, string content);
 }
 
-public class MessageService(IMessageRepository repository) : IMessageService
+public class MessageService(IMessageRepository repository, IUserService userService) : IMessageService
 {
     [LogTime]
     [LogMethodParameters]
@@ -39,8 +40,17 @@ public class MessageService(IMessageRepository repository) : IMessageService
     [LogTime]
     [LogMethodParameters]
     [LogReturnValue]
-    public Task<bool> PostMessage(string username, string content)
+    public async Task<bool> PostMessage(string username, string content)
     {
-        return repository.PostMessage(username, content);
+        try
+        {
+            return await repository.PostMessage(username, content);
+        }
+        catch (UserDoesntExistException e)
+        {
+            await userService.RegisterUserFromException(username, e);
+            return await repository.PostMessage(username, content);
+        }
+        
     }
 }
