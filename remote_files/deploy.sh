182f867e110a42554783187aa5e6c7a76eac30e4
Christoffer grünberg <gryn@itu.dk>
2025-04-13 20:42:06 +0200

    Updated deploy script
    Now creates a docker network

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -1,6 +1,8 @@
 source ~/.bash_profile
 
-cd /minitwit
+if ! docker network ls --format '{{.Name}}' | grep -w shared_network; then
+  sudo docker network create shared_network
+fi
 
 sudo docker compose -f docker-compose.yml pull
 sudo docker compose -f docker-compose.yml up -d
